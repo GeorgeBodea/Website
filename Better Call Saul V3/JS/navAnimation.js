61a123f36fde3218e4860f6a3fd03bfeefa808c4
GeorgeBodea <61265969+GeorgeBodea@users.noreply.github.com>
2020-04-03 01:21:26 +0300

    Major updates Homepage
    Added content such as videos, paragraphs and a footer. Modifed elements in such way that the site is resposive, has animation for menu in Mobile View Mode, added many text stylizastion.

--- Better Call Saul V3/JS/navAnimation.js
+++ Better Call Saul V3/JS/navAnimation.js
@@ -1,4 +1,7 @@
 function onClickHamburgerMenu()
 {
     document.getElementById("Entire_Menu").classList.toggle("Class_For_Animation");
+
+    document.getElementById("Entire_Menu_Background").classList.toggle("Entire_Menu_Background_Animation");
+
 }
